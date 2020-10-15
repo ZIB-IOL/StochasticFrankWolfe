71184091962ce511a486f769d2019c8f550dd39c
Christoph Spiegel <christophspiegel.berlin@gmail.com>
2020-10-15 15:05:57 +0200

    setup.py for tensorflow package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name            = "ZIB-IOL",
+    version         = "1.0.0",
+    author          = "IOL Lab @ TUB/ZIB",
+    author_email    = "spiegel@zib.de",
+    description     = "Pytorch and Tensorflow implementations of Stochastic Franl-Wolfe methods",
+    url             = "github.com/ZIB-IOL/StochasticFrankWolfe",
+    packages        = setuptools.find_packages(),
+    classifiers     = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+)
